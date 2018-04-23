9347b91625f2e00dcc8645c96812b03c4b71fc37
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ VB/WebSite/Default.aspx.vb
@@ -0,0 +1,28 @@
+﻿Imports System
+
+Partial Public Class _Default
+    Inherits System.Web.UI.Page
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As EventArgs)
+        If (Not IsPostBack) AndAlso (Not IsCallback) Then
+            Session("ViewVisible") = False
+        End If
+    If Session("ViewVisible") IsNot Nothing Then
+            htmlEditor.Settings.AllowHtmlView = DirectCast(Session("ViewVisible"), Boolean)
+            htmlEditor.Settings.AllowPreview = DirectCast(Session("ViewVisible"), Boolean)
+    End If
+    End Sub
+
+    Protected Sub cp_Callback(ByVal sender As Object, ByVal e As DevExpress.Web.ASPxClasses.CallbackEventArgsBase)
+        If e.Parameter = "opened" Then
+            htmlEditor.Settings.AllowHtmlView = True
+            htmlEditor.Settings.AllowPreview = True
+            Session("ViewVisible") = True
+        End If
+        If e.Parameter = "closed" Then
+            htmlEditor.Settings.AllowHtmlView = False
+            htmlEditor.Settings.AllowPreview = False
+            Session("ViewVisible") = False
+        End If
+    End Sub
+End Class
